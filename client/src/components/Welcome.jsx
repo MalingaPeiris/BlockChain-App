842027c2e619d606d2e098f5f386e36727668fad
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-02-20 10:33:48 +0530

    welcome-heading

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,6 +1,26 @@
+import { AiFillAlipayCircle } from "react-icons/ai";
+import { SiEthereum } from "react-icons/Si";
+import { BsInfoCircle } from "react-icons/bs";
+
+import { Loader } from "./";
+
 const Welcome = () => {
     return (
-        <h1>Welcome</h1>
+        <div className="flex w-full justify-center items-center " >
+            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col md:mr-10">
+                    <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
+                        Send Crypto <br /> across the world
+                    </h1>
+                    <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
+                        Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+                    </p>
+                    <button>
+
+                    </button>
+                </div>
+            </div>
+        </div>
     );
 }
 
