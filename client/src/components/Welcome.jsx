1d7a632769c165ce4c200331f4b6bf2774bcc5b9
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2022-03-23 19:39:10 +0530

    welcome-pending

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -7,13 +7,23 @@ import { Loader } from "./";
 const commonStyles =
   "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
-const Input = () => <input placeholder={placeholder} />;
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="0.0001"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm outline-none p-2 bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
 const Welcome = () => {
   const connectwallet = () => {};
+  const handleSubmit = () => {};
   return (
     <div className="flex w-full justify-center items-center ">
-      <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start flex-col md:mr-10">
+      <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+        <div className="flex flex-1 justify-start flex-col mf:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
@@ -38,7 +48,7 @@ const Welcome = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-col flex-1 justify-start items-center w-full md:mt-0 mt-10">
+      <div className="flex flex-col flex-1 justify-start items-center w-full mf:mt-0 mt-10">
         <div className="p-3 justify-end items-center flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card white-glassmorphism">
           <div className="flex justify-between flex-col w-full h-full">
             <div className="flex justify-between items-start">
@@ -60,6 +70,36 @@ const Welcome = () => {
             type="text"
             handleChange={() => {}}
           />
+          <Input
+            placeholder="Amount (ETH)"
+            name="amount"
+            type="number"
+            handleChange={() => {}}
+          />
+          <Input
+            placeholder="Keyword (GIF)"
+            name="keyword"
+            type="text"
+            handleChange={() => {}}
+          />
+          <Input
+            placeholder="Enter Message"
+            name="message"
+            type="text"
+            handleChange={() => {}}
+          />
+          <div className="h-[1px] w-full bg-gray-400 my-2" />
+          {false ? (
+            <Loader />
+          ) : (
+            <button
+              type="button"
+              onClick={handleSubmit}
+              className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] rounded-full cursor-pointer"
+            >
+              Send Now
+            </button>
+          )}
         </div>
       </div>
     </div>
